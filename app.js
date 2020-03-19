ac8b7d27d9d872db84180a92e928ed85e82a563f
Pedro Perez <pedrojperez1@gmail.com>
2020-03-19 14:31:20 -0400

    simplified code base by removing updateStorage() function

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ if (localStorage.tasks) { // load in existing HTML that was in storage, if it ex
 todo.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") { // if click on task, mark as done and update storage
         e.target.classList.toggle("done");
-        updateStorage();
+        localStorage.setItem("tasks", todo.innerHTML);
     } else if (e.target.tagName === "BUTTON") { // if click on button, remove parent and update storage
         e.target.parentElement.remove();
-        updateStorage();
+        localStorage.setItem("tasks", todo.innerHTML);
     }
 })
 
@@ -21,7 +21,7 @@ input.addEventListener("submit", function(e) {
     e.preventDefault();
     let newTask = createNewTodoElement(newTodo.value);
     todo.prepend(newTask); // add new task when submit is clicked and then update storage
-    updateStorage(); 
+    localStorage.setItem("tasks", todo.innerHTML);
     newTodo.value = '';
 })
 
@@ -32,9 +32,4 @@ function createNewTodoElement(text) {
     newBtn.innerText = "Remove";
     newLi.append(newBtn);
     return newLi;
-}
-
-
-function updateStorage() {
-    localStorage.setItem("tasks", todo.innerHTML);
 }
